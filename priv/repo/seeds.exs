69b75f45c20a78517549dc07db040c9522b78203
Hung Tran <trannguyenhung011086@gmail.com>
2026-03-17 23:28:42 +0700

    seed delivery options

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias DeliveryApp.Repo
+alias DeliveryApp.Orders.DeliveryOption
+
+Repo.insert!(%DeliveryOption{
+  code: "STANDARD",
+  name: "Standard Delivery",
+  eta_days: 3,
+  base_fee_cents: 500,
+  active: true
+})
+
+Repo.insert!(%DeliveryOption{
+  code: "EXPRESS",
+  name: "Express Delivery",
+  eta_days: 1,
+  base_fee_cents: 1500,
+  active: true
+})
